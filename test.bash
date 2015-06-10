df80dc70f8d89757c4df5aeefb77efbadc72f1fa
Hiroaki Saito <a1526hs@aiit.ac.jp>
2015-06-11 08:42:41 +0900

    ver.2

--- test.bash
+++ test.bash
@@ -2,18 +2,21 @@
 
 #TEST1
 VAR=$(bash 15745126 38456 32)
+echo $VAR
 if [ ! "$VAR" -eq "8" ]; then
  exit 10
 fi
 
 #TEST2
 VAR2=$(bash 15745126 1000 2)
+echo $VAR2
 if [ ! "$VAR2" -eq "2" ]; then
  exit 10
 fi
 
 #TEST3
 VAR3=$(bash 15745126 150 360)
+echo $VAR3
 if [ ! "$VAR3" -eq "30" ]; then
  exit 10
 fi
