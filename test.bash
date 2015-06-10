87c1fc25a8da98d657de7c1b869265b80b2552f9
Hiroaki Saito <a1526hs@aiit.ac.jp>
2015-06-11 03:46:42 +0900

    3rd commit

--- test.bash
+++ test.bash
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-echo "This is Test."
-echo "AAAAA"
+#TEST1
+VAR=$(bash 15745126 38456 32)
+if [ ! "$VAR" -eq "8" ]; then
+ exit 10
+fi
+
+#TEST2
+VAR2=$(bash 15745126 1000 2)
+if [ ! "$VAR2" -eq "2" ]; then
+ exit 10
+fi
+
+#TEST3
+VAR3=$(bash 15745126 150 360)
+if [ ! "$VAR3" -eq "30" ]; then
+ exit 10
+fi
